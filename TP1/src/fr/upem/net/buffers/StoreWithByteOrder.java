db5f69d427c705150d0ccd0c76b78b28e0000cfe
Alexandre-Gb <giboz.alexandre@gmail.com>
2024-03-21 16:39:13 +0100

    Exo1-2

---
+++ TP1/src/fr/upem/net/buffers/StoreWithByteOrder.java
@@ -0,0 +1,53 @@
+package fr.upem.net.buffers;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class StoreWithByteOrder {
+
+	public static void usage() {
+		System.out.println("StoreWithByteOrder [LE|BE] filename");
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			usage();
+			return;
+		}
+
+		var path = Path.of(args[1]);
+		var buffer = ByteBuffer.allocate(Long.BYTES);
+		switch (args[0].toUpperCase()) {
+			case "LE":
+				buffer.order(ByteOrder.LITTLE_ENDIAN);
+				break;
+			case "BE": // By default Big endian
+				break;
+			default:
+				System.out.println("Unrecognized option : " + args[0]);
+				usage();
+				return;
+		}
+
+		try (var outChannel = FileChannel.open(path, WRITE, CREATE, TRUNCATE_EXISTING);
+				var scanner = new Scanner(System.in)) {
+			while (scanner.hasNextLong()) {
+				if (buffer.remaining() < Long.BYTES) {
+					buffer.flip();
+					outChannel.write(buffer);
+					buffer.clear();
+				}
+				buffer.putLong(scanner.nextLong());
+			}
+
+			buffer.flip();
+			outChannel.write(buffer);
+		}
+	}
+}
