2e86aac785b321853e0bdd29bb392db4cd08ed4c
Alexandre-Gb <giboz.alexandre@gmail.com>
2024-03-21 18:11:47 +0100

    ex3

---
+++ TP1/src/fr/upem/net/buffers/ReadStandardInputWithEncoding.java
@@ -0,0 +1,42 @@
+package fr.upem.net.buffers;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.charset.Charset;
+
+public class ReadStandardInputWithEncoding {
+  private static final int BUFFER_SIZE = 1024;
+
+  private static void usage() {
+    System.out.println("Usage: ReadStandardInputWithEncoding charset");
+  }
+
+  private static String stringFromStandardInput(Charset cs) throws IOException {
+    var buffer = ByteBuffer.allocate(BUFFER_SIZE);
+    var nbRead = 0;
+
+    ReadableByteChannel in = Channels.newChannel(System.in);
+    while ((nbRead = in.read(buffer)) != -1) {
+      if (buffer.remaining() < nbRead) {
+        var newBuffer = ByteBuffer.allocate(buffer.capacity() * 2);
+        buffer.flip();
+        newBuffer.put(buffer);
+        buffer = newBuffer;
+      }
+    }
+
+    buffer.flip();
+    return cs.decode(buffer).toString();
+  }
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+      usage();
+      return;
+    }
+    Charset cs = Charset.forName(args[0]);
+    System.out.print(stringFromStandardInput(cs));
+  }
+}
