ac6fe585be23123cf4f0bc0afd886b72e7b3e5de
Alexandre-Gb <alexandre.giboz@edu.univ-eiffel.fr>
2024-05-06 14:56:05 +0200

    exo1-2

---
+++ TP10/src/fr/upem/net/tcp/nonblocking/ServerSum.java
@@ -0,0 +1,121 @@
+package fr.upem.net.tcp.nonblocking;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.logging.Logger;
+
+public class ServerSum {
+
+	private static final int BUFFER_SIZE = 2 * Integer.BYTES;
+	private static final Logger logger = Logger.getLogger(ServerSum.class.getName());
+
+	private final ServerSocketChannel serverSocketChannel;
+	private final Selector selector;
+
+	public ServerSum(int port) throws IOException {
+		serverSocketChannel = ServerSocketChannel.open();
+		serverSocketChannel.bind(new InetSocketAddress(port));
+		selector = Selector.open();
+	}
+
+	public void launch() throws IOException {
+		serverSocketChannel.configureBlocking(false);
+		serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+		while (!Thread.interrupted()) {
+			try {
+				Helpers.printKeys(selector); // for debug
+				System.out.println("Starting select");
+				selector.select(this::treatKey);
+				System.out.println("Select finished");
+			} catch (UncheckedIOException tunnel) {
+				throw tunnel.getCause();
+			}
+		}
+	}
+
+	private void treatKey(SelectionKey key) {
+		Helpers.printSelectedKey(key); // for debug
+		try {
+			if (key.isValid() && key.isAcceptable()) {
+				doAccept(key);
+			}
+			if (key.isValid() && key.isWritable()) {
+				doWrite(key);
+			}
+			if (key.isValid() && key.isReadable()) {
+				doRead(key);
+			}
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
+
+	private void doAccept(SelectionKey key) throws IOException {
+		var sc = serverSocketChannel.accept();
+		if (sc == null) {
+			logger.info("Connection refused");
+			return;
+		}
+
+		sc.configureBlocking(false);
+		sc.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(Integer.BYTES * 2));
+		// var clientKey = sc.register(selector, SelectionKey.OP_READ, ByteBuffer.allocate(Integer.BYTES * 2));
+		// clientKey.attach(ByteBuffer.allocate(Integer.BYTES * 2));
+	}
+
+	private void doRead(SelectionKey key) throws IOException {
+		var sc = (SocketChannel) key.channel();
+		var bb = (ByteBuffer) key.attachment();
+		bb.clear();
+		if (sc.read(bb) == -1) {
+			silentlyClose(key);
+			return;
+		}
+		if (bb.hasRemaining()) {
+			return;
+		}
+
+		bb.flip();
+		var sum = bb.getInt() + bb.getInt();
+		logger.info("Sum is " + sum);
+		bb.clear();
+		bb.putInt(sum);
+		bb.flip();
+		key.interestOps(SelectionKey.OP_WRITE);
+	}
+
+	private void doWrite(SelectionKey key) throws IOException {
+		var sc = (SocketChannel) key.channel();
+		var bb = (ByteBuffer) key.attachment();
+		sc.write(bb);
+		if (bb.hasRemaining()) {
+			return;
+		}
+
+		key.interestOps(SelectionKey.OP_READ);
+	}
+
+	private void silentlyClose(SelectionKey key) {
+		var sc = (Channel) key.channel();
+		try {
+			sc.close();
+		} catch (IOException e) {
+			// ignore exception
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		if (args.length != 1) {
+			usage();
+			return;
+		}
+		new ServerSum(Integer.parseInt(args[0])).launch();
+	}
+
+	private static void usage() {
+		System.out.println("Usage : ServerSumOneShot port");
+	}
+}
